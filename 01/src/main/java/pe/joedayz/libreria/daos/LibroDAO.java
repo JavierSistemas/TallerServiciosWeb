dbb643696cd9d31a4bec712a0ce2e5d3a29a3fde
JavierSistemas <jreyes.sistemas@outlook.com>
2021-12-28 19:40:30 -0500

    cambios 1

--- 01/src/main/java/pe/joedayz/libreria/daos/LibroDAO.java
+++ 01/src/main/java/pe/joedayz/libreria/daos/LibroDAO.java
@@ -10,12 +10,21 @@ public class LibroDAO {
 
 	public List<Libro> listarLibros() {
 		List<Libro> libros = new ArrayList<Libro>();
-		libros.add(new Libro(2012, new ArrayList<String>(Arrays.asList("Maximo Perez", 
-				"Minimo Perez")), "Atlas", "Guia del Programador", 
-				"La biblia del programador ..."));
-		libros.add(new Libro(2012, new ArrayList<String>(Arrays.asList("Rudy Rails")), 
-				"RubyPeru", "Ruby on Rails", "Cree rapidamente aplicaciones web"));
+		libros.add(new Libro(2012, new ArrayList<String>(Arrays.asList("Maximo Perez", "Minimo Perez")), "Atlas", "Guia del Programador","La biblia del programador ..."));
+		libros.add(new Libro(2012, new ArrayList<String>(Arrays.asList("Rudy Rails")), "RubyPeru", "Ruby on Rails", "Cree rapidamente aplicaciones web"));
 		return libros;
+	}
+	
+	public List<Libro> listarLibros(Integer numeroDePagina, Integer tamanioDePagina){
+		
+		List<Libro> libros = listarLibros();
+		int indiceInicial = numeroDePagina * tamanioDePagina;
+		int indiceFinal = indiceInicial + tamanioDePagina;
+		
+		indiceFinal = indiceFinal > libros.size() ? libros.size() : indiceFinal;
+		indiceInicial = indiceInicial > indiceFinal ? indiceFinal : indiceInicial;
+		
+		return libros.subList(indiceInicial, indiceFinal);
 	}
 
 }
--- 01/src/main/java/pe/joedayz/libreria/servicios/ListaLibros.java
+++ 01/src/main/java/pe/joedayz/libreria/servicios/ListaLibros.java
@@ -2,9 +2,9 @@ package pe.joedayz.libreria.servicios;
 
 import java.util.List;
 
+import javax.jws.WebResult;
 import javax.jws.WebService;
 import javax.xml.ws.Endpoint;
-
 import pe.joedayz.libreria.daos.LibroDAO;
 import pe.joedayz.libreria.modelos.Libro;
 //Todos los métodos public , serán los del servicio.
@@ -12,10 +12,15 @@ import pe.joedayz.libreria.modelos.Libro;
 @WebService  //JAX-WS
 public class ListaLibros {
 
-	
+	@WebResult(name = "libro")
 	public List<Libro> listarLibros() {
 		LibroDAO libroDAO = obtenerDAO();
 		return libroDAO.listarLibros();
+	}
+	
+	public List<Libro>  listarLibros(int numeroDePagina, int  tamanioDePagina){
+		LibroDAO libroDao = new LibroDAO();
+		return libroDao.listarLibros(numeroDePagina, tamanioDePagina);
 	}
 	
 	private LibroDAO obtenerDAO() {
